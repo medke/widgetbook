72ed60efd7efde5cdf0d4b0c76918cfe6502f86e
Mohamed <mohamed.kettouch@tchibo.de>
2024-09-03 16:39:16 +0200

    wip

--- widgetbook/lib/ui/widgets/primary_button.dart
+++ widgetbook/lib/ui/widgets/primary_button.dart
@@ -7,3 +7,15 @@ import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
 /// Add Widgetbook use-case here
+@UseCase(
+  name: 'Default',
+  type: PrimaryButton,
+  designLink:
+      'https://www.figma.com/design/HsANkdhbsCNTkXBzNJRNLD/Groceries-Demo?node-id=7340-24471&node-type=INSTANCE&t=wKZvIrLsTHq853Zm-0',
+)
+Widget buildIconButtonUseCase(BuildContext context) {
+  return PrimaryButton(
+    onPressed: () {},
+    content: context.knobs.string(label: 'content', initialValue: 'Widgetbook Workshop'),
+  );
+}
